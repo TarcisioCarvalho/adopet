5ea807dbc5990dffc74b38dab26da7b73b1b0d42
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-07-12 19:57:07 -0300

    responsividade tela inicial

--- src/Components/Inicial/index.js
+++ src/Components/Inicial/index.js
@@ -14,7 +14,7 @@ const Inicial = () =>{
             {/*  <img src={logo} alt=''/>
             <h2>Boas Vindas!</h2>
             <p>Que tal mudar sua vida adotando seu novo melhor amigo? Vem com a gente!</p> */}
-            <Logo titulo = 'Boas Vindas!' paragrafo = 'Que tal mudar sua vida adotando seu novo melhor amigo? Vem com a gente!'/>
+            <Logo logoBranca = {true} titulo = 'Boas Vindas!' paragrafo = 'Que tal mudar sua vida adotando seu novo melhor amigo? Vem com a gente!'/>
             <Button pagina = 'login'>Já tenho conta</Button>
             <Button pagina= 'cadastro'>Quero me cadastrar</Button> 
         </section>
--- src/Components/Logo/index.js
+++ src/Components/Logo/index.js
@@ -3,15 +3,17 @@ import logoBranca from '../../assets/img/logo.svg'
 import logoAzul from '../../assets/img/logo-azul.png'
 
 
-
+let logo = logoAzul;
 
 
 const Logo = (props) =>{
-   
+   if(props.logoBranca){
+    logo = logoBranca;
+   }
     
     return(
         <>
-            <img src={logoAzul} className = 'logo_img' alt='Logo do Adopet'/>
+            <img src={logo} className = 'logo_img' alt='Logo do Adopet'/>
             <h2 className='logo__titulo'>{props.titulo}</h2>
             <p className='logo__paragrafo'>{props.paragrafo}</p>
         </>
