abd4a6746025c972d4a1abe9be6a4f85aa6499ed
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-01 21:20:20 -0300

    Controllers Criados.

--- backend/AdopetAPI/Controllers/UsuarioController.cs
+++ backend/AdopetAPI/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
 using AdopetAPI.Data;
+using AdopetAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdopetAPI.Controllers;
 
@@ -12,12 +14,62 @@ public class UsuarioController:ControllerBase
     {
         return Ok();
     }
-     [HttpGet("usuarios")]
-    public IActionResult GetUsuarios(
+     [HttpGet("v1/usuarios")]
+    public async Task<IActionResult> GetUsuariosAsync(
         [FromServices] AdopetDbContext context
     )
     {
-        var usuarios = context.Usuarios.ToList();
+        var usuarios = await context.Usuarios.ToListAsync();
         return Ok(usuarios);
+    }
+
+      [HttpGet("v1/usuarios/{id:int}")]
+    public async Task<IActionResult> GetUsuarioIdAsync(
+        [FromRoute] int id,
+        [FromServices] AdopetDbContext context
+    )
+    {
+        var usuario = await context.
+        Usuarios.
+        FirstOrDefaultAsync(x => x.Id == id);
+
+        if(usuario == null) return NotFound();
+
+        return Ok(usuario);
+    }
+    [HttpPost("v1/usuarios")]
+    public async Task<IActionResult> PostUsuarioAsync(
+        [FromBody] Usuario usuario,
+        [FromServices] AdopetDbContext context
+    )
+    {
+        await context.Usuarios.AddAsync(usuario);
+        await context.SaveChangesAsync();
+
+        
+
+        return Created($"v1/usuarios/{usuario.Id}",usuario);
+    }
+    [HttpPut("v1/usuarios/{id:int}")]
+    public async Task<IActionResult> PutUsuarioAsync(
+        [FromBody] Usuario modelo,
+        [FromRoute] int id,
+        [FromServices] AdopetDbContext context
+    )
+    {
+        var usuario = await context.
+        Usuarios.
+        FirstOrDefaultAsync(x => x.Id == id);
+
+        if(usuario == null) return NotFound();
+
+        usuario.Cidade = modelo.Cidade;
+        usuario.Sobre = modelo.Sobre;
+        usuario.EnderecoFotoPerfil = modelo.EnderecoFotoPerfil;
+
+        context.Usuarios.Update(usuario);
+        await context.SaveChangesAsync();
+
+        return Ok(usuario);
     }
 }
