085ff9f9da77d6769f31729c59dba1a70dcd0d00
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-04 15:42:45 -0300

    Validações do viewModel com Anottations

--- backend/AdopetAPI/ViewModels/CreateUsuarioViewModel.cs
+++ backend/AdopetAPI/ViewModels/CreateUsuarioViewModel.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AdopetAPI.ViewModels;
 
 public class CreateUsuarioViewModel
 {
+    [Required(ErrorMessage ="o nome é obrigatório")]
+    [StringLength(40,MinimumLength =3,ErrorMessage ="O Nome deve conter entre 3 e 80 caracteres.")]
     public string Nome { get; set; }
+
+    [Required(ErrorMessage ="o eE-mail é obrigatório")]
+    [EmailAddress(ErrorMessage ="E-mail inválido")]
     public string Email { get; set; }
     
 }
