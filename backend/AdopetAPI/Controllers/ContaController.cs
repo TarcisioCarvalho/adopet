9cd83da9eef933ce2d6e1af400d17b0b320f31b1
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-15 18:34:53 -0300

    Criando Controlador de conta

---
+++ backend/AdopetAPI/Controllers/ContaController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdopetAPI.Controllers;
+
+[ApiController]
+public class ContaController:ControllerBase
+{
+    [HttpPost("v1/login")]
+    public IActionResult Login()
+    {
+        
+    } 
+}
