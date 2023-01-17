b531f3d4e2b472250a73c83fb62e97cd73e32acb
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-17 16:09:33 -0300

    Injeçao de Dependencia AddTransient

--- backend/AdopetAPI/Program.cs
+++ backend/AdopetAPI/Program.cs
@@ -1,9 +1,11 @@
 using AdopetAPI.Data;
+using AdopetAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AdopetDbContext>();
+builder.Services.AddTransient<TokenService>();
 
 
 
