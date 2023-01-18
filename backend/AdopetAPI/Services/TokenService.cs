d94251e4bba1b4284cdf3af345a1ca02fed5da97
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-18 15:29:31 -0300

    Clain Identity

--- backend/AdopetAPI/Services/TokenService.cs
+++ backend/AdopetAPI/Services/TokenService.cs
@@ -1,4 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using AdopetAPI.Models;
 using Microsoft.IdentityModel.Tokens;
@@ -9,10 +10,15 @@ public class TokenService
 {
     public string GenerateToken(Usuario usuario)
     {
+        
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
         var tokenDescriptor  = new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new (ClaimTypes.Name,value:"Tarcísio")
+            }),
             Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key),
