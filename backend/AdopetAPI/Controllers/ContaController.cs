de81466bb0437fa5771bcb0a445fadbd0bd23e16
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-21 20:17:14 -0300

    Comecando a Registrar Usuario

--- backend/AdopetAPI/Controllers/ContaController.cs
+++ backend/AdopetAPI/Controllers/ContaController.cs
@@ -1,4 +1,6 @@
+using AdopetAPI.Data;
 using AdopetAPI.Services;
+using AdopetAPI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdopetAPI.Controllers;
@@ -6,7 +8,17 @@ namespace AdopetAPI.Controllers;
 [ApiController]
 public class ContaController:ControllerBase
 {
-    [HttpPost("v1/login")]
+    [HttpPost("v1/contar")]
+    public async Task<IActionResult> Post(
+        [FromBody] RegistrarViewModel model,
+        [FromServices] AdopetDbContext context 
+    )
+    {
+        // Parei aki
+        if(!ModelState.IsValid)
+            return BadRequest();
+    }
+    [HttpPost("v1/contas/login")]
     public IActionResult Login([FromServices] TokenService tokenService)
     {
         var token = tokenService.GenerateToken(null);
---
+++ backend/AdopetAPI/ViewModels/RegistrarViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AdopetAPI.ViewModels;
+
+public class RegistrarViewModel
+{
+    [Required(ErrorMessage ="Nome é Obrigatório")]
+    public string Nome { get; set; }
+    
+    [Required(ErrorMessage ="O E-mail é Obrigatório")]
+    [EmailAddress(ErrorMessage ="O Email é Inválido")]
+    public string Email { get; set; }
+}
