1a6e972553e453af9794e6a39b97ca1eed77bd46
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-01-25 17:38:40 -0300

    Salvando Password no Banco

--- backend/AdopetAPI/Controllers/ContaController.cs
+++ backend/AdopetAPI/Controllers/ContaController.cs
@@ -1,8 +1,11 @@
 using AdopetAPI.Data;
 using AdopetAPI.Extensions;
+using AdopetAPI.Models;
 using AdopetAPI.Services;
 using AdopetAPI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace AdopetAPI.Controllers;
 
@@ -19,7 +22,35 @@ public class ContaController:ControllerBase
         if(!ModelState.IsValid)
             return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
 
-        return Ok();
+        var usuario = new Usuario
+        {
+          Nome = model.Nome,
+          Email = model.Email,
+          
+        };
+
+        var password = PasswordGenerator.Generate(25);
+        usuario.Senha = PasswordHasher.Hash(password);
+
+        try
+        {
+            await context.Usuarios.AddAsync(usuario);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                usuario = usuario.Email, password
+            }));
+        }
+        catch (DbUpdateException)
+        {
+            
+            return StatusCode(400,new ResultViewModel<string>("05x99 - Este E-mail já está cadastrado"));
+        }
+        catch
+        {
+            return StatusCode(500,new ResultViewModel<string>("05x04 - Falha interna"));
+        }
     }
     [HttpPost("v1/contas/login")]
     public IActionResult Login([FromServices] TokenService tokenService)
